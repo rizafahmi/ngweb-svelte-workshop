9cb91993ff5e501da47bdb16dd81db9d06d4246c
Riza Fahmi <rizafahmi@gmail.com>
2023-01-05 09:21:10 +0700

    Template Syntax

--- src/lib/Editor.svelte
+++ src/lib/Editor.svelte
@@ -4,5 +4,17 @@
 </script>
 
 <h2>{description}</h2>
+{#if wordCount > 5}
+  <blockquote>Warning: Text too long!</blockquote>
+{:else}
+  <blockquote>Need more text</blockquote>
+{/if}
 <textarea bind:value={description} />
 <p><strong>Word count: </strong>{wordCount}</p>
+
+<style>
+  blockquote {
+    font-size: 2rem;
+    color: tomato;
+  }
+</style>
